005b17505d9f93fd015c4cbb8a02595204d6d2b7
chaiyapat_k <chaiyapat.k@allrightbox.com>
2020-06-09 19:41:28 +0700

    Payment และ Training

---
+++ Presentation/Web/SubcontractProfile.Web/Controllers/PaymentController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SubcontractProfile.Web.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult ConfirmPayment()
+        {
+            return View();
+        }
+        public IActionResult VerifyPayment()
+        {
+            return View();
+        }
+    }
+}
