48e10df2710cb85b1369a3deff96411598df52c8
kesss204 <kessada.s@extend-it-resource.com>
2020-07-23 15:42:10 +0700

    no message

--- Frameworks/SubcontractProfile.WebApi/SubcontractProfile.WebApi.IoC.Configuration/DI/ServiceCollectionExtensions.cs
+++ Frameworks/SubcontractProfile.WebApi/SubcontractProfile.WebApi.IoC.Configuration/DI/ServiceCollectionExtensions.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 using SubcontractProfile.WebApi.Services.Services;
+using Repository;
 
 namespace SubcontractProfile.WebApi.IoC.Configuration.DI
 {
@@ -15,24 +16,25 @@ namespace SubcontractProfile.WebApi.IoC.Configuration.DI
             if (services != null)
             {
                 services.AddTransient<IUserService, UserService>();
+                services.AddTransient<IDbContext, DbContext>();
                 services.AddTransient<ISubcontractProfileAddressRepo, SubcontractProfileAddressRepo>();
                 services.AddTransient<ISubcontractProfileAsstEngineerRepo, SubcontractProfileAsstEngineerRepo>();
                 services.AddTransient<ISubcontractProfileBankingRepo, SubcontractProfileBankingRepo>();
                 services.AddTransient<ISubcontractProfileCompanyRepo, SubcontractProfileCompanyRepo>();
                 services.AddTransient<ISubcontractProfileDistrictRepo, SubcontractProfileDistrictRepo>();
-                //services.AddTransient<ISubcontractProfileCompanyRepo, SubcontractProfileCompanyRepo>();
-                //services.AddTransient<ISubcontractProfileCompanyRepo, SubcontractProfileCompanyRepo>();
-                //services.AddTransient<ISubcontractProfileCompanyRepo, SubcontractProfileCompanyRepo>();
-                //services.AddTransient<ISubcontractProfileCompanyRepo, SubcontractProfileCompanyRepo>();
-                //services.AddTransient<ISubcontractProfileCompanyRepo, SubcontractProfileCompanyRepo>();
-                //services.AddTransient<ISubcontractProfileCompanyRepo, SubcontractProfileCompanyRepo>();
-                //services.AddTransient<ISubcontractProfileCompanyRepo, SubcontractProfileCompanyRepo>();
-                //services.AddTransient<ISubcontractProfileCompanyRepo, SubcontractProfileCompanyRepo>();
-                //services.AddTransient<ISubcontractProfileCompanyRepo, SubcontractProfileCompanyRepo>();
-                //services.AddTransient<ISubcontractProfileCompanyRepo, SubcontractProfileCompanyRepo>();
-                //services.AddTransient<ISubcontractProfileCompanyRepo, SubcontractProfileCompanyRepo>();
-                //services.AddTransient<ISubcontractProfileCompanyRepo, SubcontractProfileCompanyRepo>();
-                //services.AddTransient<ISubcontractProfileCompanyRepo, SubcontractProfileCompanyRepo>();
+                services.AddTransient<ISubcontractProfileEngineerRepo, SubcontractProfileEngineerRepo>();
+                services.AddTransient<ISubcontractProfileLocationRepo, SubcontractProfileLocationRepo>();
+                services.AddTransient<ISubcontractProfileNationalityRepo, SubcontractProfileNationalityRepo>();
+                services.AddTransient<ISubcontractProfilePaymentRepo, SubcontractProfilePaymentRepo>();
+                services.AddTransient<ISubcontractProfilePersonalRepo, SubcontractProfilePersonalRepo>();
+                services.AddTransient<ISubcontractProfileProvinceRepo, SubcontractProfileProvinceRepo>();
+                services.AddTransient<ISubcontractProfileRaceRepo, SubcontractProfileRaceRepo>();
+                services.AddTransient<ISubcontractProfileReligionRepo, SubcontractProfileReligionRepo>();
+                services.AddTransient<ISubcontractProfileSubDistrictRepo, SubcontractProfileSubDistrictRepo>();
+                services.AddTransient<ISubcontractProfileTeamRepo, SubcontractProfileTeamRepo>();
+                services.AddTransient<ISubcontractProfileTitleRepo, SubcontractProfileTitleRepo>();
+                services.AddTransient<ISubcontractProfileTrainingRepo, SubcontractProfileTrainingRepo>();
+                services.AddTransient<ISubcontractProfileUserRepo, SubcontractProfileUserRepo>();
                 //services.AddTransient<ISubcontractProfileCompanyRepo, SubcontractProfileCompanyRepo>();
                 //services.AddTransient<ISubcontractProfileCompanyRepo, SubcontractProfileCompanyRepo>();
                 //services.AddTransient<ISubcontractProfileCompanyRepo, SubcontractProfileCompanyRepo>();
