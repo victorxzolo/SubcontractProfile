c06f23a4dc5ba151c10cb8a696eea3fa7ec35e4d
kesss204 <kessada.s@extend-it-resource.com>
2020-07-29 12:36:08 +0700

    no message

--- Frameworks/SubcontractProfile.WebApi/SubcontractProfile.WebApi.API/Controllers/AsstEngineerController.cs
+++ Frameworks/SubcontractProfile.WebApi/SubcontractProfile.WebApi.API/Controllers/AsstEngineerController.cs
@@ -13,7 +13,7 @@ using SubcontractProfile.WebApi.Services.Model;
 
 namespace SubcontractProfile.WebApi.API.Controllers
 {
-    [Route("api/AddressTypeController")]
+    [Route("api/AsstEngineerController")]
     [ApiController]
     public class AsstEngineerController : ControllerBase
     {
