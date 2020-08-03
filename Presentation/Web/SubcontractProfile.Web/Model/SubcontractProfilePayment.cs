395001b557218734136a70e1ce591c6f8a1550a0
chaiyapat_k <chaiyapat.k@allrightbox.com>
2020-08-03 21:11:14 +0700

    get data

---
+++ Presentation/Web/SubcontractProfile.Web/Model/SubcontractProfilePayment.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SubcontractProfile.Web.Model
+{
+    public class SubcontractProfilePayment
+    {
+        public string PaymentId { get; set; }
+
+        [System.ComponentModel.DataAnnotations.StringLength(100)]
+        public string PaymentNo { get; set; }
+
+        [System.ComponentModel.DataAnnotations.StringLength(100)]
+        public string PaymentChannal { get; set; }
+
+        public System.DateTime? PaymentDatetime { get; set; }
+
+        public decimal? AmountTransfer { get; set; }
+
+        [System.ComponentModel.DataAnnotations.StringLength(100)]
+        public string BankTransfer { get; set; }
+
+        [System.ComponentModel.DataAnnotations.StringLength(100)]
+        public string BankBranch { get; set; }
+
+        public string SlipAttachFile { get; set; }
+
+        public string Remark { get; set; }
+
+        public System.DateTime? CreateDate { get; set; }
+
+        [System.ComponentModel.DataAnnotations.StringLength(50)]
+        public string CreateBy { get; set; }
+
+        [System.ComponentModel.DataAnnotations.StringLength(50)]
+        public string ModifiedBy { get; set; }
+
+        public System.DateTime? ModifiedDate { get; set; }
+
+        public System.Guid TrainingId { get; set; }
+
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
