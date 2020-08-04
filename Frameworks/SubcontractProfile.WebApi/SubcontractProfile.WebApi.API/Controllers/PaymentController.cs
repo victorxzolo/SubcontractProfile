19924d0c916a8a698323cd6b39ea5a9e999fb562
kesss204 <kessada.s@extend-it-resource.com>
2020-08-04 23:52:31 +0700

    no message

--- Frameworks/SubcontractProfile.WebApi/SubcontractProfile.WebApi.API/Controllers/PaymentController.cs
+++ Frameworks/SubcontractProfile.WebApi/SubcontractProfile.WebApi.API/Controllers/PaymentController.cs
@@ -74,23 +74,23 @@ namespace SubcontractProfile.WebApi.API.Controllers
         [ProducesResponseType(StatusCodes.Status204NoContent, Type = typeof(SubcontractProfilePayment))]
         [HttpGet("SearchPayment/{payment_no}/{request_training_no}/{request_date_from}/{request_date_to}/{payment_date_from}/{payment_date_to}/{payment_status}")]
         public Task<IEnumerable<SubcontractProfile.WebApi.Services.Model.SubcontractProfilePayment>> SearchPayment(string payment_no,
-           string request_training_no, string request_date_from, string request_date_to, string payment_date_from,
+           string request_training_no , string request_date_from, string request_date_to, string payment_date_from,
            string payment_date_to, string payment_status)
         {
             _logger.LogInformation($"Start PaymentController::SearchPayment", payment_no, request_training_no, request_date_from,
                 request_date_to, payment_date_from, payment_date_to, payment_status);
         
-            if(payment_no =="NULL")
+            if(payment_no.ToUpper() == "NULL")
             {
                 payment_no = "";
             }
 
-            if (request_training_no == "NULL")
+            if (request_training_no.ToUpper() == "NULL")
             {
                 request_training_no = "";
             }
 
-            if (payment_status == "NULL")
+            if (payment_status.ToUpper() == "NULL")
             {
                 payment_status = "";
             }
