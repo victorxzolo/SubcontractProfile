ed921a55389521339f42fad6c67e05148cc87b08
chaiyapat_k <chaiyapat.k@allrightbox.com>
2020-08-10 19:03:18 +0700

    confirmpayment

--- Presentation/Web/SubcontractProfile.Web/Controllers/PaymentController.cs
+++ Presentation/Web/SubcontractProfile.Web/Controllers/PaymentController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -25,8 +26,8 @@ namespace SubcontractProfile.Web.Controllers
         }
         public IActionResult ConfirmPayment()
         {
-            ViewData["Controller"] = "Payment";
-            ViewData["View"] = "ConfirmPayment";
+            //ViewData["Controller"] = "Payment";
+            //ViewData["View"] = "ConfirmPayment";
             return View();
         }
         public IActionResult VerifyPayment()
@@ -53,17 +54,56 @@ namespace SubcontractProfile.Web.Controllers
         [HttpGet]
         public IActionResult Searchconfirmpayment(SubcontractProfileSearchPayment searchPayment)
         {
+
+            searchPayment.Paymentno = (string.IsNullOrEmpty(searchPayment.Paymentno)) ? "null" : searchPayment.Paymentno;
+            searchPayment.Paymentrequesttraningno = (string.IsNullOrEmpty(searchPayment.Paymentrequesttraningno)) ? "null" : searchPayment.Paymentrequesttraningno;
+            searchPayment.Paymantrequestdatefrom = (string.IsNullOrEmpty(searchPayment.Paymantrequestdatefrom)) ? "null" : searchPayment.Paymantrequestdatefrom;
+            searchPayment.Paymentrequestdateto = (string.IsNullOrEmpty(searchPayment.Paymentrequestdateto)) ? "null" : searchPayment.Paymentrequestdateto;
+            searchPayment.Paymentdatefrom = (string.IsNullOrEmpty(searchPayment.Paymentdatefrom)) ? "null" : searchPayment.Paymentdatefrom;
+            searchPayment.Paymentdateto = (string.IsNullOrEmpty(searchPayment.Paymentdateto)) ? "null" : searchPayment.Paymentdateto;
+
+
+
+            var data = new List<SubcontractProfilePayment>();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            string uriString = string.Format("{0}/{1}/{2}/{3}/{4}/{5}/{6}/{7}", strpathAPI + "Payment/SearchPayment", searchPayment.Paymentno, searchPayment.Paymentrequesttraningno, searchPayment.Paymantrequestdatefrom, searchPayment.Paymentrequestdateto, searchPayment.Paymentdatefrom, searchPayment.Paymentdateto, searchPayment.Paymentstatus);
+            HttpResponseMessage response = client.GetAsync(uriString).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var dataresponse = response.Content.ReadAsStringAsync().Result;
+                data = JsonConvert.DeserializeObject<List<SubcontractProfilePayment>>(dataresponse);
+            }
+            return Json(new { Data = data });
+        }
+        [HttpGet]
+        public IActionResult GetByPaymentId(string id)
+        {
             var data = new SubcontractProfilePayment();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            string x = "http://localhost:66/api/Payment/SearchPayment/null/null/2020-10-7/2020-10-7/2020-12-12/2020-12-12/null";
-            //string uriString = string.Format("{0}", strpathAPI + "Payment/SearchPayment");
-            HttpResponseMessage response = client.GetAsync(x).Result;
+            string uriString = string.Format("{0}/{1}", strpathAPI + "Payment/GetByPaymentId",id);
+            HttpResponseMessage response = client.GetAsync(uriString).Result;
             if (response.IsSuccessStatusCode)
             {
                 var dataresponse = response.Content.ReadAsStringAsync().Result;
                 data = JsonConvert.DeserializeObject<SubcontractProfilePayment>(dataresponse);
             }
             return Json(new { Data = data });
+
+        }
+        [HttpPost]
+        public IActionResult Inserpayment(SubcontractProfilePayment Payment)
+        {            
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));            
+            string uriString = string.Format("{0}", strpathAPI + "Payment/Insert");
+            var httpContent = new StringContent(JsonConvert.SerializeObject(Payment), Encoding.UTF8, "application/json");
+            HttpResponseMessage response = client.PostAsync(uriString, httpContent).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var dataresponse = response.Content.ReadAsStringAsync().Result;
+                
+            }
+
+            return Json(new { Date = "" });
         }
     }
 }
