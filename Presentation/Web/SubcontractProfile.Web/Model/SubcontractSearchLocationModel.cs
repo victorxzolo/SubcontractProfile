8bdbc6e902f7d32d6fe44f5981864c547934fcb2
nattaphon phaungmanee <nattaphon.p@allrightbox.com>
2020-08-20 15:00:14 +0700

    profile

---
+++ Presentation/Web/SubcontractProfile.Web/Model/SubcontractSearchLocationModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SubcontractProfile.Web.Model
+{
+    public class SubcontractSearchLocationModel
+    {
+        public System.Guid CompanyId { get; set; }
+
+        [System.ComponentModel.DataAnnotations.StringLength(100)]
+        public string LocationCode { get; set; }
+
+        [System.ComponentModel.DataAnnotations.StringLength(1000)]
+        public string LocationNameTh { get; set; }
+
+        [System.ComponentModel.DataAnnotations.StringLength(1000)]
+        public string LocationNameEn { get; set; }
+
+        [System.ComponentModel.DataAnnotations.StringLength(50)]
+
+        public string Phone { get; set; }
+
+    }
+}
