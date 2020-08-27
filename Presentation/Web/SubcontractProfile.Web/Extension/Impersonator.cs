acc0a47abd2fc1224d29ba415b4bd7fb31b9cd31
kesss204 <kessada.s@extend-it-resource.com>
2020-08-27 21:29:04 +0700

    no message

---
+++ Presentation/Web/SubcontractProfile.Web/Extension/Impersonator.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+
+namespace SubcontractProfile.Web.Extension
+{
+    public class Impersonator : IDisposable
+    {
+        protected const int LOGON32_PROVIDER_DEFAULT = 0;
+        protected const int LOGON32_LOGON_INTERACTIVE = 2;
+
+        public WindowsIdentity Identity = null;
+        private System.IntPtr m_accessToken;
+
+
+        [System.Runtime.InteropServices.DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool LogonUser(string lpszUsername, string lpszDomain,
+        string lpszPassword, int dwLogonType, int dwLogonProvider, ref System.IntPtr phToken);
+
+        [System.Runtime.InteropServices.DllImport("kernel32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+        private extern static bool CloseHandle(System.IntPtr handle);
+
+
+        // AccessToken ==> this.Identity.AccessToken
+        //public Microsoft.Win32.SafeHandles.SafeAccessTokenHandle AT
+        //{
+        //    get
+        //    {
+        //        var at = new Microsoft.Win32.SafeHandles.SafeAccessTokenHandle(this.m_accessToken);
+        //        return at;
+        //    }
+        //}
+
+        public Impersonator()
+        {
+            this.Identity = WindowsIdentity.GetCurrent();
+        }
+
+
+        public Impersonator(string username, string password,string domainOrServerName, bool useDomain = true)
+        {
+            Login(username, domainOrServerName, password);
+        }
+
+
+        public void Login(string username, string domain, string password)
+        {
+            if (this.Identity != null)
+            {
+                this.Identity.Dispose();
+                this.Identity = null;
+            }
+
+
+            try
+            {
+                this.m_accessToken = new System.IntPtr(0);
+                Logout();
+
+                this.m_accessToken = System.IntPtr.Zero;
+                bool logonSuccessfull = LogonUser(
+                   username,
+                   domain,
+                   password,
+                   LOGON32_LOGON_INTERACTIVE,
+                   LOGON32_PROVIDER_DEFAULT,
+                   ref this.m_accessToken);
+
+                if (!logonSuccessfull)
+                {
+                    int error = System.Runtime.InteropServices.Marshal.GetLastWin32Error();
+                    throw new System.ComponentModel.Win32Exception(error);
+                }
+                Identity = new WindowsIdentity(this.m_accessToken);
+            }
+            catch
+            {
+                throw;
+            }
+
+        } // End Sub Login 
+
+        public void Logout()
+        {
+            if (this.m_accessToken != System.IntPtr.Zero)
+                CloseHandle(m_accessToken);
+
+            this.m_accessToken = System.IntPtr.Zero;
+
+            if (this.Identity != null)
+            {
+                this.Identity.Dispose();
+                this.Identity = null;
+            }
+
+        } // End Sub Logout 
+
+        void System.IDisposable.Dispose()
+        {
+            Logout();
+        } // End Sub Dispose 
+
+        #region external dll functions
+
+        public const int LOGON32_LOGON_NETWORK = 3;
+        public const int LOGON32_LOGON_BATCH = 4;
+        public const int LOGON32_LOGON_SERVICE = 5;
+        public const int LOGON32_LOGON_UNLOCK = 7;
+        public const int LOGON32_LOGON_NETWORK_CLEARTEXT = 8;
+        public const int LOGON32_LOGON_NEW_CREDENTIALS = 9;
+
+        public const int LOGON32_PROVIDER_WINNT35 = 1;
+        public const int LOGON32_PROVIDER_WINNT40 = 2;
+        public const int LOGON32_PROVIDER_WINNT50 = 3;
+
+
+    
+        #endregion external dll functions
+
+
+    }
+}
